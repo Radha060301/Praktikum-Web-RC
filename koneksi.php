ee7e666283d8242ee7ac16593dc309ed926232cd
Radha060301 <60578400+Radha060301@users.noreply.github.com>
2021-11-01 22:43:00 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+
+$host="localhost";
+$user="root";
+$pass="";
+$database="latihan_db";
+
+//$koneksi = mysqli_connect($host, $user, $pass);
+$koneksi = mysqli_connect("localhost","root","","latihan_db");
+
+?>
